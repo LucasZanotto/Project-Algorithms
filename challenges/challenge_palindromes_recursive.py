f5fc6411cfbb7f7331a6452e6b9925b235d0f437
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-02-17 18:18:55 -0300

    feat: função que verifica se é um palindromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    palavra = word
+    tamanho = len(palavra) // 2
+    if not palavra:
+        return False
+    if low_index == tamanho:
+        return True
+    if palavra[high_index] != palavra[low_index]:
+        return False
+    else:
+        return is_palindrome_recursive(word, (low_index + 1), (high_index - 1))
+
+
+print(is_palindrome_recursive("AGUA", 0, 3))
